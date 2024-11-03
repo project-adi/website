20f42b7e9749971b654627d97e210bc714a7564d
lolguy91 <retek1544@gmail.com>
2024-11-03 13:49:08 +0100

    HUGE MAKEOVER

--- assets/pageSwitcher.js
+++ assets/pageSwitcher.js
@@ -43,15 +43,28 @@ function fillPage(){
 	if(fragment == ""){
         fragment = "home";
         document.title = "Project ADI: The Next Generation Driver Interface";
-    } else {
-        document.title = "Project ADI - " + fragment.charAt(0).toUpperCase() + fragment.slice(1);
-
+    } else if(fragment == "cp") {
+		document.title = "Project ADI - Cookie Policy";
+	} else if (fragment == "pp"){
+		document.title = "Project ADI - Privacy Policy";
+	} else {
+        document.title = "Project ADI - " + fragment.charAt(0).toUpperCase() + fragment.slice(1).replaceAll("_", " ");
     }
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4) {
 			if (this.status == 200) {contents.innerHTML = this.responseText;}
-			if (this.status == 404) {contents.innerHTML = "Page not found.";}
+			if (this.status == 404) {
+				var newxhttp = new XMLHttpRequest();
+				newxhttp.onreadystatechange = function() {
+					if (this.readyState == 4) {
+						if (this.status == 200) {contents.innerHTML = this.responseText;}
+						if (this.status == 404) {contents.innerHTML = "BRO WHAT THE FUCK NOT EVEN THE 404 PAGE IS LOADING FOR YA";}
+					}
+				}
+				newxhttp.open("GET", "pages/404.html", true);
+				newxhttp.send();
+			}
 		}
 	}
 	xhttp.open("GET", "pages/" + fragment + ".html", true);
